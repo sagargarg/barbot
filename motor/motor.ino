f9b0422eb737b5bc584fd293a24354d181895b82
sagargarg <sagargarg@gmail.com>
2015-01-17 10:27:53 -0500

    Two Motors at once

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,18 +1,25 @@
-int motorPin = 9;
+int motorPin1 = 2;
+int motorPin2 = 4;
+
 void setup() {
-  pinMode(motorPin, OUTPUT);
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
 }
   
 void loop() {
- digitalWrite(motorPin, HIGH);
+ digitalWrite(motorPin1, HIGH);
+ digitalWrite(motorPin2, HIGH);
  delay(100);
   
- digitalWrite(motorPin, LOW);
+ digitalWrite(motorPin1, LOW);
+ digitalWrite(motorPin2, LOW);
  delay(1000);
  
- digitalWrite(motorPin, HIGH);
+ digitalWrite(motorPin1, HIGH);
+ digitalWrite(motorPin2, HIGH);
  delay(100);
  
- digitalWrite(motorPin, LOW);
+ digitalWrite(motorPin1, LOW);
+ digitalWrite(motorPin2, LOW);
  delay(200);
 }
