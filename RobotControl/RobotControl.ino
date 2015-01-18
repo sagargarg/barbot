06db175fb3a99455aea627d7d67e17f6dc52ab48
Shaantam Chawla <shaantam_c@yahoo.com>
2015-01-18 09:46:25 -0500

    added open/close top functions

--- RobotControl/RobotControl.ino
+++ RobotControl/RobotControl.ino
@@ -1,115 +1,42 @@
 #include <MeetAndroid.h>
 #include <Servo.h>
+
+MeetAndroid meetAndroid;
 Servo myservo;
 
-#define LEFTSIDE_NEG   2
-#define LEFTSIDE_POS   3
-#define RIGHTSIDE_NEG  6
-#define RIGHTSIDE_POS  7
 #define TRIGGER_PIN    8
 #define ECHO_PIN       12
 
-MeetAndroid meetAndroid;
-
 void setup(){
- /* myservo.attach(11);
+  myservo.attach(11);
   int pos = myservo.read();
+  
   //set servo at to 180 degrees to begin with
-//  while (pos != 180) {
-//    myservo.write(pos);
-//    delay(15);
-//    pos++;
-}*/
-  
-  
- 
- Serial.begin(9600); 
- 
- meetAndroid.registerFunction(forward, 'w');
- meetAndroid.registerFunction(backward, 's');
- meetAndroid.registerFunction(leftTurn, 'a');
- meetAndroid.registerFunction(rightTurn, 'd');
- meetAndroid.registerFunction(stopIt, 'x');
-
- 
-  pinMode(LEFTSIDE_NEG, OUTPUT);
-  pinMode(LEFTSIDE_POS, OUTPUT);
-  pinMode(RIGHTSIDE_NEG, OUTPUT);
-  pinMode(RIGHTSIDE_POS, OUTPUT);
-  
+  while (pos != 180) {
+    myservo.write(pos);
+    delay(15);
+    pos++;
 }
+  
+Serial.begin(9600); 
 
+meetAndroid.registerFunction(openTop, 'w');
+meetAndroid.registerFunction(closeTop, 'x');  
+}
 
 void loop(){
-  //meetAndroid.receive();
-  
-  //while (getSonar() > 30) {
-    forward2();
-  //}
-  
-  //while (getSonar() < 30) {
-    //rightTurn2();
-    //delay(50);
-  //}
-  
-  
-  delay(100);
+  meetAndroid.receive();
   
 }
 
-void forward(byte flag, byte numOfValues)
-{
-  analogWrite(LEFTSIDE_POS, meetAndroid.getInt());
-  analogWrite(RIGHTSIDE_POS,meetAndroid.getInt());
-  analogWrite(LEFTSIDE_NEG,0);
-  analogWrite(LEFTSIDE_NEG,0);
+void openTop(byte flag, byte numOfValues) {
+  //130
+  myservo.write(meetAndroid.getInt());
 }
 
-void forward2()
-{
-  analogWrite(LEFTSIDE_POS, 255);
-  analogWrite(RIGHTSIDE_POS, 255);
-  analogWrite(LEFTSIDE_NEG,255);
-  analogWrite(LEFTSIDE_NEG,255);
-}
-
-void backward(byte flag, byte numOfValues)
-{
-  analogWrite(LEFTSIDE_POS,0);
-  analogWrite(RIGHTSIDE_POS,0);
-  analogWrite(LEFTSIDE_NEG,meetAndroid.getInt());
-  analogWrite(LEFTSIDE_NEG,meetAndroid.getInt()); 
-}
-
-
-void stopIt(byte flag, byte numOfValues)
-{
-  analogWrite(LEFTSIDE_POS,meetAndroid.getInt());
-  analogWrite(RIGHTSIDE_POS,meetAndroid.getInt());
-  analogWrite(LEFTSIDE_NEG,meetAndroid.getInt());
-  analogWrite(RIGHTSIDE_NEG,meetAndroid.getInt()); 
-}
-
-void leftTurn(byte flag, byte numOfValues){
-  analogWrite(LEFTSIDE_POS,0);
-  analogWrite(LEFTSIDE_NEG,meetAndroid.getInt());
-  analogWrite(RIGHTSIDE_POS,meetAndroid.getInt());
-  analogWrite(RIGHTSIDE_NEG,0);
-  Serial.print(meetAndroid.getInt());
-}
-
-void rightTurn(byte flag, byte numOfValues){
-   analogWrite(LEFTSIDE_POS,meetAndroid.getInt());
-   analogWrite(LEFTSIDE_NEG,0);
-   analogWrite(RIGHTSIDE_POS,0);
-   analogWrite(RIGHTSIDE_NEG,meetAndroid.getInt());  
-}
-
-void rightTurn2(){
-   analogWrite(LEFTSIDE_POS, 255);
-   analogWrite(LEFTSIDE_NEG,0);
-   analogWrite(RIGHTSIDE_POS,0);
-   analogWrite(RIGHTSIDE_NEG, 255);  
+void closeTop(byte flag, byte numOfValues) {
+  //180
+  myservo.write(meetAndroid.getInt());
 }
 
 int getSonar(){
@@ -120,7 +47,5 @@ int getSonar(){
   
   return distance;
 }
-
-
 
 
