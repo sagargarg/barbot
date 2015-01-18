4512820de2d79085091b1a0e70633433f73696b2
sagargarg <sagargarg@gmail.com>
2015-01-18 03:23:08 -0500

    Combination of servo and motors

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,9 +1,25 @@
+#include <Servo.h>
+Servo myservo;
+
 int motorPin1 = 2;
 int motorPin2 = 4;
 
 void setup() {
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
+  
+  myservo.attach(11);
+  int pos = myservo.read();
+  
+  //set servo at 45 degrees to begin with
+  while (pos != 180) {
+    myservo.write(pos);
+    delay(15);
+    pos++;
+  }
+  myservo.write(130);
+  delay(2000);
+  
 }
   
 void loop() {
@@ -22,31 +38,6 @@ void loop() {
  digitalWrite(motorPin1, LOW);
  digitalWrite(motorPin2, LOW);
  delay(200);
-}
-
-
-
-
-
-
-#include <Servo.h>
-Servo myservo;
-
-void setup() {
-  myservo.attach(11);
-  int pos = myservo.read();
-  
-  //set servo at 45 degrees to begin with
-  while (pos != 180) {
-    myservo.write(pos);
-    delay(15);
-    pos++;
-  }
-  myservo.write(130);
-  delay(2000);
-}
-  
-void loop() {
 }
 
 //opens cage to deliver drink
